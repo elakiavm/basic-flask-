f5c511bf4b6185c8a7b3ed59a0389f61664b3105
elakia <vmelakia@gmail.com>
2022-04-24 16:23:19 +0530

    update the template

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+from mmap import PROT_READ
 from flask import Flask
 
 app = Flask(__name__)
+PORT = 4538
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/",methods=["GET"])
 def start ():
     # user=['Priyanka','Elakia','Vini']
     # return render_template('index3.html',name="elakia",members=user)
 
-    return 'hello world'
+    return ('hello world')
 
 if __name__ =="__main__":
-    app.run(debug=True)
+    app.run(
+            debug=True,
+            host='0.0.0.0',
+            port= PORT 
+            )
+
+
+    
