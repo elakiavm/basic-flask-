a9d295d3cdeb6cffce9f131c6172c888c023c350
elakia <vmelakia@gmail.com>
2022-04-24 16:29:58 +0530

    update the template

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def start ():
 if __name__ =="__main__":
     app.run(
             debug=True,
-            # host='0.0.0.0',
-            port= PORT 
+            host='0.0.0.0',
+            port= 5000 
             )
 
 
